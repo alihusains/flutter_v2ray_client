9aafb1436a9cad641e8025ee8eedb30fd05f167f
alihusains <sorathiyaalihusain@gmail.com>
2026-02-14 21:45:03 +0400

    feat: export protocol classes in flutter_v2ray.dart

--- lib/flutter_v2ray.dart
+++ lib/flutter_v2ray.dart
@@ -13,6 +13,11 @@ import 'model/v2ray_status.dart';
 
 export 'model/v2ray_status.dart';
 export 'url/url.dart';
+export 'url/vmess.dart';
+export 'url/vless.dart';
+export 'url/trojan.dart';
+export 'url/shadowsocks.dart';
+export 'url/socks.dart';
 
 /// A class for managing V2Ray connections and operations.
 /// Provides methods to initialize, start, stop, and query V2Ray services.
